9b557b59077029517d58da2c36fc4a6e748e2c95
Fox Huston <fox.huston@tufts.edu>
2022-03-26 10:50:25 -0400

    first pass at fox layout

---
+++ keyboards/preonic/keymaps/fox/unicode.h
@@ -0,0 +1,11 @@
+enum unicode_names {
+    BANG,
+    IRONY,
+    SNEK
+};
+
+const uint32_t PROGMEM unicode_map[] = {
+    [BANG]  = 0x203D,  // ‽
+    [IRONY] = 0x2E2E,  // ⸮
+    [SNEK]  = 0x1F40D, // 🐍
+};
